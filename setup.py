3f8373b4f43aae4bd3bfac7a1f5bc021b63eaaf4
ataitler <ataitler@gmail.com>
2023-02-15 10:42:08 -0500

    setup and license

---
+++ setup.py
@@ -0,0 +1,40 @@
+# This file is part of rddl-repository
+
+# rddl-repository is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published by
+# the Free Software Foundation.
+
+# rddl-repository is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MIT License for more details.
+
+# You should have received a copy of the MIT License
+# along with rddl-repository. If not, see <https://opensource.org/licenses/MIT>.
+
+from setuptools import setup, find_packages
+
+setup(
+      name='rddl-repository',
+      version='0.1',
+      author="Ayal Taitler, Scott Sanner, Michael Gimelfarb",
+      author_email="ataitler@gmail.com, ssanner@mie.utoronto.ca, mike.gimelfarb@mail.utoronto.ca",
+      description="Home for all things RDDL",
+      license="MIT License",
+      url="https://github.com/ataitler/rddl-repository",
+      packages=find_packages(),
+      install_requires=['pillow>=9.2.0', 'matplotlib>=3.5.0', 'numpy>=1.22'],
+      python_requires=">=3.8",
+      include_package_data=True,
+      package_data={'': [ ]},
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
+
