9aa896c419a7c5c13b4add1e22bcc4cd7f3f781d
ataitler <ataitler@gmail.com>
2023-02-15 12:58:35 -0500

    method naming

--- rddl-repository/Manager/RDDLRepoManager.py
+++ rddl-repository/Manager/RDDLRepoManager.py
@@ -11,22 +11,22 @@ manifest = 'manifest.csv'
 
 class RDDLRepoManager:
     def __init__(self, rebuild=False):
-        self.Archiver_Dict = {}
+        self.archiver_dict= {}
         self.manager_path = os.path.dirname(os.path.abspath(__file__))
         if os.path.isfile(self.manager_path+'/manifest.csv') and not rebuild:
-            self._LoadRepo()    # load repo to dict
+            self._load_repo()    # load repo to dict
         else:
-            self._BuildRepo()   # build repo and load to dict
+            self._build_repo()   # build repo and load to dict
 
-    def ListProblems(self):
-        for key, values in self.Archiver_Dict.items():
+    def list_problems(self):
+        for key, values in self.archiver_dict.items():
             print(key + ": " + values[1])
 
-    def GetProblem(self, name):
-        if name in self.Archiver_Dict.keys():
-            return ProblemInfo(self.Archiver_Dict[name])
+    def get_problem(self, name):
+        if name in self.archiver_dict.keys():
+            return ProblemInfo(self.archiver_dict[name])
 
-    def _BuildRepo(self):
+    def _build_repo(self):
         root_path = os.path.dirname(os.path.abspath(__file__))
         path_to_manifest = os.path.join(root_path, 'manifest.csv')
         root_path = root_path.split('/')
@@ -48,7 +48,7 @@ class RDDLRepoManager:
                 d = dir.split('/')
                 module = 'Archive' + '.'.join(d)
                 mymodule = importlib.import_module(module)
-                if mymodule.info['name'] in self.Archiver_Dict.keys():
+                if mymodule.info['name'] in self.archiver_dict.keys():
                     raise RDDLRepoProblemDuplication()
                 if 'domain.rddl' not in files:
                     raise RDDLRepoDomainNotExist()
@@ -58,7 +58,7 @@ class RDDLRepoManager:
                 if context:
                     context = '_' + context
                 name = mymodule.info['name'] + context
-                self.Archiver_Dict[name] = [mymodule.info['name'],
+                self.archiver_dict[name] = [mymodule.info['name'],
                                                         mymodule.info['description'],
                                                         root,
                                                         instances,
@@ -72,21 +72,21 @@ class RDDLRepoManager:
             writer.writerow(HEADER)
 
             # iterate through the dictionary
-            for keys, values in self.Archiver_Dict.items():
+            for keys, values in self.archiver_dict.items():
                 values[3] = ','.join(values[3])
                 writer.writerow(values)
 
-    def _LoadRepo(self):
+    def _load_repo(self):
         root_path = os.path.dirname(os.path.abspath(__file__))
         path_to_manifest = os.path.join(root_path, 'manifest.csv')
         if not os.path.isfile(path_to_manifest):
             return {}
 
-        self.Archiver_Dict = {}
+        self.archiver_dict= {}
         with open(path_to_manifest) as file:
             reader = csv.reader(file, delimiter=',')
             for i, row in enumerate(reader):
                 if i > 0:
                     name, *entries = row
-                    self.Archiver_Dict[name] = dict(zip(HEADER[1:], entries))
-            return self.Archiver_Dict
+                    self.archiver_dict[name] = dict(zip(HEADER[1:], entries))
+            return self.archiver_dict
