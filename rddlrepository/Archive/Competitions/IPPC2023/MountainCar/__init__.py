c7b20ba3dd276f627a4554b57a6712f9f2361e10
ataitler <ataitler@gmail.com>
2023-08-24 23:27:08 -0400

    mountaincar and hvac 2023 domains

---
+++ rddlrepository/Archive/Competitions/IPPC2023/MountainCar/__init__.py
@@ -0,0 +1,8 @@
+info = {
+    'name': 'MountainCar',
+    'description': 'A simple continuous MDP for the classical mountain car control problem',
+    'context': 'ippc2023',
+    'tags': '',
+    'viz': 'MountainCarViz.MountainCarVisualizer',
+    'generator': 'MountainCarInstanceGenerator.MountainCarInstanceGenerator'
+}
