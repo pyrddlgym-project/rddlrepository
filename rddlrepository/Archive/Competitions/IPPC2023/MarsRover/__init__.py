c96100f96612bb61b122f027b5da29e4a43a8262
ataitler <ataitler@gmail.com>
2023-08-24 23:31:18 -0400

    MarsRover 2023 competition domain added

---
+++ rddlrepository/Archive/Competitions/IPPC2023/MarsRover/__init__.py
@@ -0,0 +1,8 @@
+info = {
+    'name': 'MarsRover',
+    'description': 'Multi Rover Navigation, where a group of agent needs to harvest mineral',
+    'context': 'ippc2023',
+    'tags': '',
+    'viz': 'MarsRoverViz.MarsRoverVisualizer',
+    'generator': 'MarsRoverInstanceGenerator.MarsRoverInstanceGenerator'
+}
