df55c7581afab5d61b04dabb484ebc8f5068c534
ataitler <ataitler@gmail.com>
2023-08-24 23:42:24 -0400

    recsim 2023 competition domain added

---
+++ rddlrepository/Archive/Competitions/IPPC2023/RecSim/__init__.py
@@ -0,0 +1,8 @@
+info = {
+    'name': 'RecSim',
+    'description': 'A problem of recommendation systems, with consumers and providers',
+    'context': 'ippc2023',
+    'tags': '',
+    'viz': 'RecSimViz.RecSimVisualizer',
+    'generator': 'RecSimInstanceGenerator.RecSimInstanceGenerator'
+}
