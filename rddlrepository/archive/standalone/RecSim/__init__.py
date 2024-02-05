a4f45bba8e79a4e3193a67da7a92f5523322641e
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-04 20:42:51 -0500

    Removed recsim from standalone

--- rddlrepository/archive/standalone/RecSim/__init__.py
+++
@@ -1,7 +0,0 @@
-info = {
-    'name': 'RecSim',
-    'description': 'A problem of recommendation systems, with consumers and providers.',
-    'context': '',
-    'tags': '',
-    'viz': 'RecSimViz.RecSimVisualizer'
-}
