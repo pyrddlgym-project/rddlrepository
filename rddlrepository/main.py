86b94c1cf536414d56082a327fadab7a97ea948f
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-09 16:20:57 -0500

    Update main.py

--- rddlrepository/main.py
+++ rddlrepository/main.py
@@ -5,31 +5,28 @@ def main():
 
     # initializer repo manager, repo manifest will be loaded or built automatically if not present.
     # rebuild argument force manifest rebuild.
-    # manager = RDDLRepoManager()
     manager = RDDLRepoManager(rebuild=True)
     
     # list all problems in the repo, unique name is generated based on the name and context of the problem
     print(manager.list_problems())
 
-    manager.list_contexts()
-
-    manager.list_problems_by_context('ippc2018')
+    for context in manager.list_contexts():
+        print(f'context {context}: {manager.list_problems_by_context(context)}')
 
     # getting a specific problem from the repo.
-    # envInfo = manager.get_problem('RaceCar')
     envInfo = manager.get_problem('EarthObservation_ippc2018')
 
     # list available instances in for the problem.
-    envInfo.list_instances()
+    print(envInfo.list_instances())
 
     # return path to the domain file to be used/open by external tools.
-    envInfo.get_domain()
+    print(envInfo.get_domain())
 
     # return path to the desired instance file to be used/open by external tools.
-    envInfo.get_instance(1)
+    print(envInfo.get_instance(1))
 
     # return the pyRDDLGym visualizer object (pyRDDLGym dependency required) if exists, otherwise return None.
-    envInfo.get_visualizer()
+    print(envInfo.get_visualizer())
 
 
 if __name__ == '__main__':
