b17402548a4abfba578f211eaed1a45220d447e9
ataitler <ataitler@gmail.com>
2024-02-10 00:44:39 -0500

    setup updated for v2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
       name='rddlrepository',
-      version='0.31',
+      version='2.0',
       author="Ayal Taitler, Michael Gimelfarb, Scott Sanner",
       author_email="ataitler@gmail.com, mike.gimelfarb@mail.utoronto.ca, ssanner@mie.utoronto.ca",
       description="Home for all things RDDL",
