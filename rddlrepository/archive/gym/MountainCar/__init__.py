08c6a1f0a7c67d2f7041788b906c409128bc8f28
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-11 04:58:12 -0500

    Added continuous and discrete version of mountain car

--- rddlrepository/archive/gym/MountainCar/__init__.py
+++
@@ -1,7 +0,0 @@
-info = {
-    'name': 'MountainCar',
-    'description': 'A simple continuous MDP for the classical mountain car control problem.',
-    'context': 'gym',
-    'tags': '',
-    'viz': 'MountainCarViz.MountainCarVisualizer'
-}
