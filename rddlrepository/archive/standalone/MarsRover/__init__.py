abc3eb2c01f2b99efff9b79bc66b0959ae146345
ataitler <ataitler@gmail.com>
2024-02-13 13:06:20 -0500

    MarRover removed from standalone

--- rddlrepository/archive/standalone/MarsRover/__init__.py
+++
@@ -1,7 +0,0 @@
-info = {
-    'name': 'MarsRover',
-    'description': 'Multi Rover Navigation, where a group of agent needs to harvest mineral.',
-    'context': '',
-    'tags': '',
-    'viz': 'MarsRoverViz.MarsRoverVisualizer'
-}
