e5faa340390903cd12ecb2837d26cf4591b4a236
ataitler <ataitler@gmail.com>
2024-02-13 12:52:25 -0500

    RecSim and Wildfire removed from standalone

--- rddlrepository/archive/standalone/Wildfire/__init__.py
+++
@@ -1,7 +0,0 @@
-info = {
-    'name': 'Wildfire',
-    'description': 'A boolean version of the wildfire fighting domain.',
-    'context': '',
-    'tags': '',
-    'viz': 'WildfireViz.WildfireVisualizer'
-}
