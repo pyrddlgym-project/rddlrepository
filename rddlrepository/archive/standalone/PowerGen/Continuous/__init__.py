0940743fd360234255b93fb634529aa287082452
ataitler <ataitler@gmail.com>
2024-02-12 19:47:15 -0500

    manager fix, clean up and powergen continuous addition

---
+++ rddlrepository/archive/standalone/PowerGen/Continuous/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'PowerGen_Continuous',
+    'description': 'A simple continuous version of the power generation problem, loosely modeled on the problem of unit commitment.',
+    'context': '',
+    'tags': '',
+    'viz': 'PowerGenViz.PowerGenVisualizer'
+}
--- rddlrepository/archive/standalone/PowerGen/__init__.py
+++
@@ -1,8 +0,0 @@
-
-info = {
-    'name': 'PowerGen',
-    'description': 'A simple power generation problem loosely modeled on the problem of unit commitment.',
-    'context': '',
-    'tags': '',
-    'viz': 'PowerGenViz.PowerGenVisualizer'
-}
