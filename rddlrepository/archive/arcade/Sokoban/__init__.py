ed08114bf1d10999f8fb2d128a6413b70795a0c8
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-16 22:54:31 -0500

    Added Sokoban domain to arcade

---
+++ rddlrepository/archive/arcade/Sokoban/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'Sokoban',
+    'description': 'A person pushes boxes in a warehouse onto designated storage areas, difficult domain due to dead ends.',
+    'context': 'arcade',
+    'tags': '',
+    'viz': 'None'
+}
