f8e5479c46d47779dc6badee7b81c2464410720e
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-18 19:26:37 -0500

    Added online bin packing problem

---
+++ rddlrepository/archive/standalone/BinPacking/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'BinPacking',
+    'description': 'Reject or place items into a bin to maximize total value of all items while keeping the total weight of each bin within limits.',
+    'context': '',
+    'tags': '',
+    'viz': 'None'
+}
