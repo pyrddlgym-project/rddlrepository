7186a96bd6cbf79f94d21ae718bfd3fdbc2ec305
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-20 21:14:46 -0500

    Added blackjack game

---
+++ rddlrepository/archive/arcade/Blackjack/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'Blackjack',
+    'description': 'A card is randomly drawn from a deck and added to the player hand, the goal is to stop with the highest value of the hand without going over 21.',
+    'context': 'arcade',
+    'tags': '',
+    'viz': 'None'
+}
