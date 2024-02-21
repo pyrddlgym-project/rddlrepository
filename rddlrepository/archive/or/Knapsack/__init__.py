dbcaa034f1999f9b2220e9deb6a4da0db940247b
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-20 20:10:41 -0500

    Added knapsack OR problem

---
+++ rddlrepository/archive/or/Knapsack/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'Knapsack',
+    'description': 'Items of random weight and value are drawn, the goal is to place them into knapsacks of limited total weight while maximizing total value of all items.',
+    'context': 'or',
+    'tags': '',
+    'viz': 'None'
+}
