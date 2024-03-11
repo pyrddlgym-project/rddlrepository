878b37c0317272a734bf962728550700adbd51ae
Ilia Smirnov <iliathesmirnov@gmail.com>
2024-03-11 17:20:38 -0400

    (Traffic BLX) Updated the ComplexPhases domain.rddl

---
+++ rddlrepository/archive/standalone/TrafficBLX/ComplexPhases/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'TrafficBLX_ComplexPhases',
+    'description': 'BLX/QTM traffic signal control model with a generic phasing scheme. The goal is to control traffic lights to minimize total travel time.',
+    'context': '',
+    'tags': '',
+    'viz': 'TrafficViz.TrafficVisualizer'
+}
