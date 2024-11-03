cafe2e2e62d5183c7c8e07ac85c35354ac30b532
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-11-03 06:27:03 -0500

    Change Cartpole to CartPole

--- rddlrepository/archive/gym/CartPole/Continuous/__init__.py
+++ rddlrepository/archive/gym/CartPole/Continuous/__init__.py
@@ -1,5 +1,5 @@
 info = {
-    'name': 'Cartpole_Continuous',
+    'name': 'CartPole_Continuous',
     'description': 'A simple continuous state-action MDP for the classical cart-pole system by Rich Sutton, with actions that describe the continuous force applied to the cart.',
     'context': 'gym',
     'tags': '',
--- rddlrepository/archive/gym/CartPole/Discrete/__init__.py
+++ rddlrepository/archive/gym/CartPole/Discrete/__init__.py
@@ -1,5 +1,5 @@
 info = {
-    'name': 'Cartpole_Discrete',
+    'name': 'CartPole_Discrete',
     'description': 'A simple continuous state discrete action MDP for the classical cart-pole system by Rich Sutton, with actions that describe the direction of the force applied to the cart.',
     'context': 'gym',
     'tags': '',
