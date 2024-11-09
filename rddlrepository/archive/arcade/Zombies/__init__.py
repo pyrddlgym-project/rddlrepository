43e6d40d4281d1eef7084ad29e806e3dd7cea667
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-11-09 18:34:43 -0500

    Added zombies domain and instances

---
+++ rddlrepository/archive/arcade/Zombies/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'Zombies',
+    'description': 'An epidemic game in which humans avoid becoming infected by zombies.',
+    'context': 'arcade',
+    'tags': '',
+    'viz': 'None'
+}
