93098ed2e8de6cf9302d378c5fdcf8d1cf832c34
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-11-11 05:55:10 -0500

    Added Eight domain

---
+++ rddlrepository/archive/arcade/Eight/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'Eight',
+    'description': 'Squares are moved to adjacent empty cells until a specific arrangement is achieved.',
+    'context': 'arcade',
+    'tags': '',
+    'viz': 'None'
+}
