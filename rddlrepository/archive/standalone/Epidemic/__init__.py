6e62b8d4f74c01a6051d30556a915488ee8f1d05
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2026-01-30 12:24:53 -0500

    Added epidemic control problem

---
+++ rddlrepository/archive/standalone/Epidemic/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'Epidemic',
+    'description': 'SIR model with nonlinear infection dynamics, age groups, hospitalization and multiple interventions.',
+    'context': '',
+    'tags': '',
+    'viz': 'None'
+}
